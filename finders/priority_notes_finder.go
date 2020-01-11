4fb49ab241fa07e80bf437d7aa4987656648955b
michal_urbanski <michal_urbanski@windowslive.com>
2020-01-11 19:20:52 +0100

    Refactoring - makes finding priority notes more maintainable

--- finders/priority_notes_finder.go
+++ finders/priority_notes_finder.go
@@ -8,15 +8,29 @@ import (
 // FindPriorityNotes finds those notes that are very important or important.
 // They should be acted on as a priority.
 func FindPriorityNotes(actionList *models.ActionList) *models.ActionList {
+	seek := []enums.Importance{enums.Important, enums.VeryImportant}
+
+	return findNotesWithImportance(actionList, seek)
+}
+
+func findNotesWithImportance(actionList *models.ActionList, importance []enums.Importance) *models.ActionList {
 	var priorityNotes []models.Note
 
 	for _, note := range actionList.Notes {
-		// TODO: notes types can be passed as parameter
-		if note.Importance == enums.Important ||
-			note.Importance == enums.VeryImportant {
+		if contains(importance, note.Importance) {
 			priorityNotes = append(priorityNotes, note)
 		}
 	}
 
 	return &models.ActionList{Notes: priorityNotes}
+}
+
+func contains(statuses []enums.Importance, current enums.Importance) bool {
+	for _, a := range statuses {
+		if a == current {
+			return true
+		}
+	}
+
+	return false
 }
